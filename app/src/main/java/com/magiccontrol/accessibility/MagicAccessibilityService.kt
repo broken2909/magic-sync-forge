bad37bd607f5b9c1375c7a1bf3746b7ae5e4f08a
broken2909 <broken2909@users.noreply.github.com>
2025-09-22 23:57:18 +0200

    🔧 Fix MagicAccessibilityService - add missing onBind method

--- app/src/main/java/com/magiccontrol/accessibility/MagicAccessibilityService.kt
+++ app/src/main/java/com/magiccontrol/accessibility/MagicAccessibilityService.kt
@@ -1,12 +1,35 @@
 package com.magiccontrol.accessibility
+
 import android.accessibilityservice.AccessibilityService
 import android.view.accessibility.AccessibilityEvent
+
 class MagicAccessibilityService : AccessibilityService() {
-    companion object { var instance: MagicAccessibilityService? = null }
-    override fun onServiceConnected() { super.onServiceConnected(); instance = this }
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {}
+    
+    companion object {
+        var instance: MagicAccessibilityService? = null
+    }
+    
+    override fun onServiceConnected() {
+        super.onServiceConnected()
+        instance = this
+    }
+    
+    override fun onAccessibilityEvent(event: AccessibilityEvent) {}
+    
     override fun onInterrupt() {}
-    override fun onDestroy() { super.onDestroy(); instance = null }
-    fun goBack() { performGlobalAction(GLOBAL_ACTION_BACK) }
-    fun goHome() { performGlobalAction(GLOBAL_ACTION_HOME) }
+    
+    override fun onBind(intent: android.content.Intent?): android.os.IBinder? = null
+    
+    override fun onDestroy() {
+        super.onDestroy()
+        instance = null
+    }
+    
+    fun goBack() {
+        performGlobalAction(GLOBAL_ACTION_BACK)
+    }
+    
+    fun goHome() {
+        performGlobalAction(GLOBAL_ACTION_HOME)
+    }
 }
