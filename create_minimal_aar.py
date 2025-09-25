1fd6c3580f6fc929d0b39f4740e9d237a7e5b7d1
broken2909 <broken2909@users.noreply.github.com>
2025-09-25 14:58:01 +0200

    🧹 CLEAN: Remove unnecessary files and fix repository configuration

--- create_minimal_aar.py
+++
@@ -1,9 +0,0 @@
-import zipfile
-import os
-
-with zipfile.ZipFile('libs/vosk-android.aar', 'w') as zf:
-    zf.writestr('AndroidManifest.xml', '<?xml version="1.0" encoding="utf-8"?><manifest package="org.vosk"/>')
-    zf.writestr('classes.jar', '')
-    zf.writestr('R.txt', '')
-    
-print("AAR minimal créé - 1024 bytes")
