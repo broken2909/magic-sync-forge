f1a31e4c7c2a3fafa7cc0e85ea89bc25f151c70a
broken2909 <broken2909@users.noreply.github.com>
2025-09-28 14:42:28 +0200

    Corrections finales build: imports SettingsFragment + dépendance preference-ktx

--- app/src/main/java/com/magiccontrol/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/magiccontrol/ui/settings/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.magiccontrol.ui.settings
 
 import android.os.Bundle
 import android.util.Log
+import com.magiccontrol.ui.settings.SettingsFragment
 import androidx.appcompat.app.AppCompatActivity
 import com.magiccontrol.R
 
