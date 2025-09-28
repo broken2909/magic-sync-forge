6f6d6cb4feae2753017bc81aa3f0cd53ef65e31f
broken2909 <broken2909@users.noreply.github.com>
2025-09-29 00:06:26 +0200

    TEST: Layout ultra-simplifié sans findViewById

--- app/src/main/java/com/magiccontrol/MainActivity.kt
+++ app/src/main/java/com/magiccontrol/MainActivity.kt
@@ -11,27 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        setupToolbar()
-        setupButtons()
-        showWelcomeToast()
-    }
-
-    private fun setupToolbar() {
-        setSupportActionBar(findViewById(R.id.toolbar))
-    }
-
-    private fun setupButtons() {
-        findViewById<android.widget.ImageButton>(R.id.voice_button)?.setOnClickListener {
-            Toast.makeText(this, "Bouton vocal", Toast.LENGTH_SHORT).show()
-        }
-
-        findViewById<android.widget.Button>(R.id.settings_button)?.setOnClickListener {
-            val intent = Intent(this, com.magiccontrol.ui.settings.SettingsActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    private fun showWelcomeToast() {
-        Toast.makeText(this, R.string.welcome_message, Toast.LENGTH_LONG).show()
+        // Test simple sans findViewById
+        Toast.makeText(this, "Application démarrée", Toast.LENGTH_SHORT).show()
     }
 }
